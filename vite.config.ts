b8a2432f4b8fb35015692cca9611b6d7ef0db2ba
seohyungchoi0511-sketch <seohyungchoi0511@gmail.com>
2026-02-19 11:21:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '', '')
   return {
-    // base: '/sbepe/',   // ← 여기! repo 이름 + 슬래시 (대문자 SPEPE면 그대로)
+    base: '/sbepe/',   // ← 여기! repo 이름 + 슬래시 (대문자 SPEPE면 그대로)
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
