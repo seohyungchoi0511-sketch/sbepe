ed7f007cf32462858cbb5ecb03a6e016cea73d71
seohyung <seohyungchoi0511@gmail.com>
2026-02-26 11:22:08 +0900

    fix: base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "/",
+    base: "/sbepe/",
     plugins: [react()],
     define: {
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
