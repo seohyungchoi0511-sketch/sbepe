ea540dbda3588a5c2ebb4de3abda8f1a04350d24
seohyung <seohyungchoi0511@gmail.com>
2026-02-26 10:16:30 +0900

    fix: base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: '/sbepe/',  // 리포 이름 + 슬래시 필수!
+    base: "/",
     plugins: [react()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
     },
   };
-});
+}); 
